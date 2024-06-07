c300373d47ba9b7f4a600f505a8919e9bd48ea78
pierre_cours <pierre_cours@MacBook-Pro-80.local>
2024-06-07 10:11:04 +0200

    Wip

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -7,9 +7,15 @@ export default function ProfilePage() {
   return (
     <Layout>
 
+    <div className={ProfileStyles.profile_banner}>
+      <img src="https://images.unsplash.com/photo-1510228957367-be4a85ea7509?q=80&w=3000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+    </div>
+
       <div className={ProfileStyles.profile}>
         <div className={ProfileStyles.profile_card}>
-          <div className={ProfileStyles.profile_image}></div>
+          <div className={ProfileStyles.profile_image}>
+            <img src="https://images.unsplash.com/photo-1517849845537-4d257902454a?q=80&w=3024&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+          </div>
           <div className={ProfileStyles.profile_infos}>
             <div className={ProfileStyles.profile_infos_name}>Fabio Courquin</div>
             <div className={ProfileStyles.profile_infos_age}>22 ans</div>

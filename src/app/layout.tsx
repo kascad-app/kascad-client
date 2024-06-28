10b9ac71c7a84b39451271570b26772dc843ed2d
Matis DENE <matis.dene@edu.devinci.fr>
2024-06-28 09:29:23 +0200

    update global

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Figtree } from "next/font/google";
 import "./globals.css";
+import Header from "./components/Header";
 
 const inter = Figtree({ subsets: ["latin"] });
 
@@ -16,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+      </body>
     </html>
   );
 }

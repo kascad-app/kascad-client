bf98e568cd06449a6998b090934ca5cd38695f8e
Arthur <arthurlucas92@orange.fr>
2024-06-28 11:21:18 +0200

    work in progress

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -38,6 +38,7 @@ export default function ProfilePage() {
   }, [loading, resetting]);
 
   const handleSpanClick = (index: number) => {
+    // console.log(span);
     // Update the loading state to the clicked span index
     setLoading(index); // Index is zero-based, so add 1 to match loading state
   };
@@ -58,11 +59,11 @@ export default function ProfilePage() {
           <p>@candide</p>
         </div>
         {/* <img className="w-full" src={} alt="Carousel" /> */}
-        <div className="absolute z-10 bottom-10 inset-x-1/2 h-4 flex w-fit">
+        <div className="absolute z-10 bottom-10 inset-x-1/2 transform -translate-x-1/2 h-4 flex w-fit">
           {images.map((_, index) => (
             <span
               key={index}
-              className="w-12 h-2 mx-2 rounded-full bg-white relative overflow-hidden cursor-pointer"
+              className="w-12 h-2 mx-2 rounded-full bg-white relative overflow-hidden cursor-pointer "
               onClick={() => handleSpanClick(index)}
             >
               <span
@@ -84,7 +85,43 @@ export default function ProfilePage() {
           ))}
         </div>
       </header>
-      <section className="w-full h-screen bg-white"></section>
+      <section className="w-full h-screen bg-white flex flex-col items-center p-16 font-semibold">
+        <div className="flex w-1/4 h-fit justify-evenly">
+          <p className="cursor-pointer ">PRESENTATION</p>
+          <p>|</p>
+          <p className="cursor-pointer">STATISTIQUE</p>
+          <p>|</p>
+          <p className="cursor-pointer">CONTENU</p>
+        </div>
+
+        <div className="flex w-full justify-center gap-x-32 items-center h-full pt-40">
+          <div
+            className="inset-0 bg-cover w-4/12 h-full"
+            style={{ backgroundImage: "url(./views/profile/profile.png)" }}
+          ></div>
+          <div className="w-1/4">
+            <h2 className="font-bold font-figtree text-6xl pb-12">ABOUT ME</h2>
+            <p className="text-base font-thin">
+              Skieur professionnel, freeskieur et snowboardeur, défie les
+              conventions avec son style unique et ses exploits audacieux.
+              <br />
+              <br /> Sur les pentes, il repousse les limites de l'impossible,
+              enchaînant figures acrobatiques et descentes périlleuses avec une
+              grâce inégalée. Ses vidéos virales, capturant ses prouesses, ont
+              fait de lui une icône des sports d'hiver. <br />
+              <br />
+              Son palmarès impressionnant témoigne de son talent et de sa
+              détermination. Son engagement ne se limite pas au sport, il est
+              également entrepreneur et créateur de contenu, partageant sa
+              passion avec le monde entier.
+              <br />
+              <br /> Candid Thovex, un virtuose de la neige qui inspire et
+              émerveille.
+            </p>
+          </div>
+        </div>
+      </section>
+      <section></section>
     </>
   );
 }

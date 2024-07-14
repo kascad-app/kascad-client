6d452a51b40525d384b33f4239ed79ff33c895c3
ImJustLucas <lucas.bellier@icloud.com>
2024-07-14 16:45:53 +0200

    ✨ add services dir and requester

---
+++ src/plugins/requester/request/requester.ts
@@ -0,0 +1,42 @@
+import { APIResponse, APIResponsePromise } from "@kascad-app/shared-types";
+import request, { ApiRequestConfig } from "./request";
+
+const createAuthHeaders = (jwtToken: string): HeadersInit => ({
+  Authorization: `${
+    process.env.NEXT_PUBLIC_AUTH_PREFIX || "Bearer"
+  } ${jwtToken}`,
+});
+
+type BaseRequest = <TData>(
+  url: string,
+  options?: ApiRequestConfig
+) => APIResponsePromise<TData>;
+
+type RequestResponse = {
+  get: BaseRequest;
+  post: BaseRequest;
+  put: BaseRequest;
+  delete: BaseRequest;
+};
+
+const requester = (): RequestResponse => {
+  let baseOptions: ApiRequestConfig = {};
+
+  const createRequest =
+    (method: string) =>
+    async <TData = any>(
+      url: string,
+      options?: ApiRequestConfig
+    ): Promise<APIResponse<TData>> => {
+      return request<TData>(url, { ...baseOptions, ...options, method });
+    };
+
+  return {
+    get: createRequest("GET"),
+    post: createRequest("POST"),
+    put: createRequest("PUT"),
+    delete: createRequest("DELETE"),
+  };
+};
+
+export { requester };

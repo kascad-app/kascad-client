280c42b03b8aa2c66b1353ed7e98c13fa410c351
Arthur <arthurlucas92@orange.fr>
2024-07-18 11:40:08 +0200

    middleware package fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,11 @@
-import useSession from "@hooks/use-session";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import Cookies from "js-cookie";
 import { cookies } from "next/headers";
-// import jwt from "jsonwebtoken";
-// import { verifyToken } from "./verifyToken";
-import { verifyJwtToken } from "./verifyToken";
 import { jwtVerify } from "jose";
 
-// This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
   const loggedIn: string | undefined = cookies().get("logged-in")?.value;
   const accessToken: string | undefined = cookies().get("access-token")?.value;
-  //   console.log(accessToken, loggedIn, loggedIn == "true");
   if (
     accessToken == undefined &&
     (loggedIn == "false" || loggedIn == undefined)
@@ -28,13 +21,9 @@ export async function middleware(request: NextRequest) {
       console.log(error.message);
       return NextResponse.redirect(new URL("/login", request.url));
     }
-    // if (verifiedToken.code && verifiedToken.code == "ERR_JWT_EXPIRED") {
-    //   return NextResponse.redirect(new URL("/login", request.url));
-    // }
   }
 }
 
-// See "Matching Paths" below to learn more
 export const config = {
-  matcher: "/marketplace",
+  matcher: ["/marketplace", "/profile"],
 };

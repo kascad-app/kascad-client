c84174ae79f4656664a6f8788cd13dacc266a3ca
Arthur <arthur.lucas92@orange.fr>
2024-11-22 12:03:13 +0100

    :construction: refactor onboarding component to use step-based navigation and create Step1 and Step2 components

--- app/onboarding/page.tsx
+++ app/onboarding/page.tsx
@@ -1,61 +1,29 @@
 "use client";
 import React, { useEffect, useRef, useState } from "react";
 import "./onboarding.css";
-import { Card } from "@/widgets/card-sport-onboarding";
-import { Select } from "@/shared/ui/select";
+import { Step1 } from "@/widgets/onboarding/Step1";
+import { Step2 } from "@/widgets/onboarding/Step2";
 
-const cardsData = [
-  { title: "Surf", src: "/views/onBoard/Surf.png" },
-  { title: "Skate", src: "/views/onBoard/Skate.png" },
-  { title: "Snow", src: "/views/onBoard/Snow.png" },
-  { title: "BMX", src: "/views/onBoard/BMX.png" },
-  { title: "BMX", src: "/views/onBoard/BMX.png" },
-  { title: "BMX", src: "/views/onBoard/BMX.png" },
-];
-
-const options = [
-  { label: "Value 1", value: "value1" },
-  { label: "Value 2", value: "value2" },
-  { label: "Value 3", value: "value3" },
-  { label: "Value 4", value: "value4" },
-];
+const nextStep = (step: number, setStep: (step: number) => void) => {
+  setStep(step);
+};
 
 const OnBoarding: React.FC = () => {
+  const [step, setStep] = useState(1);
   return (
     <div className="w-full max-w-screen flex h-screen max-h-full overflow-hidden relative flex items-center justify-center">
       <div className="gradient-circle z-1"></div>
       <div className="align-center p-8 w-1/2 z-10">
-        <p className="p-2 text-large text-dark-700">Step 1 of 2</p>
-        <h2 className="p-2 text-title text-dark-950 font-semibold">
-          Customize your Experience
-        </h2>
-        <Select label="Country" options={options} />
-        <h3 className="p-2 text-subtitle text-dark-950 font-medium">
-          Select your sports
-        </h3>
-
-        <p className="p-2 pb-12 text-subtitle2 text-dark-700">
-          This will help us customize your experience!
-        </p>
-        <div
-          className="flex px-4 pb-2 gap-6 w-full max-w-full overflow-x-auto scrollbar-hide"
-          id="style-1"
-        >
-          {cardsData.map((card, index) => (
-            <Card key={index} title={card.title} src={card.src} />
-          ))}
-        </div>
-
-        <span className="h-0.5 block  w-full bg-dark-300 my-8"></span>
-        <h3 className="p-2 text-subtitle text-dark-950 font-medium">
-          How we communicate
-        </h3>
-        <div className="flex flex-row gap-8">
-          <Select label="Country" options={options} />
-          <Select label="Email adress" options={options} />
-        </div>
+        <p className="p-2 text-large text-dark-700">Step {step} of 2</p>
+        {step === 1 && <Step1 />}
+        {step === 2 && <Step2 />}
         <div className="w-full flex justify-end">
-          <button className="bg-blue-600 text-white py-2 rounded-lg px-8 mt-8">
+          <button
+            onClick={() => {
+              nextStep(step + 1, setStep);
+            }}
+            className="bg-blue-600 text-white py-2 rounded-lg px-8 mt-8"
+          >
             Next
           </button>
         </div>

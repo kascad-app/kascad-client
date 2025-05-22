f6f10a3b948d690ce9ceab8e3f565662721001fa
Pierre CAILLET <pierre.caillet@edu.devinci.fr>
2025-05-22 17:47:08 +0200

    🚧 Nav

--- app/components/BottomNav.tsx
+++ app/components/BottomNav.tsx
@@ -4,6 +4,9 @@ import { Button } from "@/components/ui/button";
 export default function BottomNav() {
     return (
         <div className="fixed bottom-6 left-1/2 -translate-x-1/2 bg-white border border-black px-6 py-2 rounded-full flex gap-4 shadow-md z-50">
+            <Link href="/home">
+                <img className="h-10" src="/views/logos/logoSVG-blue.svg" alt="" />
+            </Link>
             <Link href="/profil">
                 <Button variant="ghost" className="text-black text-sm">Mon profil</Button>
             </Link>

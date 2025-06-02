71a395e78719c55eba4aff7fea8014f72c6a3350
Matis DENE <matis.dene@edu.devinci.fr>
2025-06-02 16:25:41 +0200

    fix merge

--- src/entities/riders/riders.hooks.ts
+++ src/entities/riders/riders.hooks.ts
@@ -1,8 +1,8 @@
 import useSWRMutation from "swr/mutation";
 
-import { Rider } from "@kascad-app/shared-types";
 import { sendSWRRequest } from "@/lib/swr/use-swr";
 import { SWR_KEY } from "@/shared/constants/SWR_KEY";
+import { Rider } from "@kascad-app/shared-types";
 import useSWR from "swr";
 import { requester } from "@/lib/requester/requester";
 
@@ -19,8 +19,16 @@ export function useUpdateOne(id: string) {
   );
 }
 
+
+export function useGetRiders() {
+  return useSWR<Rider[]>(SWR_KEY.RIDER.RIDERS, () =>
+    requester().get<Rider[]>(SWR_KEY.RIDER.RIDERS)
+  );
+}
+
+
 export function useGetRider(slug: string) {
-  return useSWR<Rider>(SWR_KEY.RIDER.RIDER, () =>
+  return useSWR<Rider>(SWR_KEY.RIDER.RIDER(slug), () =>
     requester().get<Rider>(SWR_KEY.RIDER.RIDER(slug)),
   );
 }

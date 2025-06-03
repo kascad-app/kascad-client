e34df5494552622c2353470e0eae70c2da32d1fb
Matis DENE <matis.dene@edu.devinci.fr>
2025-06-03 11:57:53 +0200

    refactor: clean up BottomNav component by removing merge conflict markers

--- app/components/BottomNav.tsx
+++ app/components/BottomNav.tsx
@@ -5,15 +5,14 @@ import { ROUTES } from "@/shared/constants/ROUTES";
 export default function BottomNav() {
   return (
     <div className="fixed bottom-6 left-1/2 -translate-x-1/2 bg-white border border-black px-6 py-2 rounded-full flex gap-4 shadow-md z-50">
-<<<<<<< HEAD
+
       <Link href="/home">
         <img className="h-10" src="/views/logos/logoSVG-blue.svg" alt="" />
-=======
+      </Link>
       <Link href={ROUTES.HOMEPAGE}>
         <Button variant="ghost" className="text-black text-sm">
           Accueil
         </Button>
->>>>>>> abe66e93d60986e3acb884ee92d95b2a0cf25023
       </Link>
       <Link href={ROUTES.RIDER.PROFILE}>
         <Button variant="ghost" className="text-black text-sm">

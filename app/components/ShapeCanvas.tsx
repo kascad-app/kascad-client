561a63c2e76c5af88932c37023a5672b457cdd01
Pierre CAILLET <pierre.caillet@edu.devinci.fr>
2025-07-23 17:30:26 +0200

    🚧 Wip Portfolio

--- app/components/ShapeCanvas.tsx
+++ app/components/ShapeCanvas.tsx
@@ -1,6 +1,6 @@
-'use client';
+"use client";
 
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef } from "react";
 
 interface ShapeCanvasProps {
   className?: string;
@@ -8,7 +8,11 @@ interface ShapeCanvasProps {
   canvasHeight?: number;
 }
 
-const ShapeCanvas: React.FC<ShapeCanvasProps> = ({ className = '', canvasWidth, canvasHeight }) => {
+const ShapeCanvas: React.FC<ShapeCanvasProps> = ({
+  className = "",
+  canvasWidth,
+  canvasHeight,
+}) => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const previousVerticesRef = useRef<{ x: number; y: number }[] | null>(null);
   const lastDrawTimeRef = useRef<number>(performance.now());
@@ -17,24 +21,24 @@ const ShapeCanvas: React.FC<ShapeCanvasProps> = ({ className = '', canvasWidth, 
   useEffect(() => {
     const canvas = canvasRef.current;
     if (!canvas) return;
-  
-    const ctx = canvas.getContext('2d');
+
+    const ctx = canvas.getContext("2d");
     if (!ctx) return;
-  
+
     let animationFrameId: number;
-  
+
     const draw = () => {
       animationFrameId = requestAnimationFrame(draw);
-  
+
       const currentTime = performance.now();
       const elapsedTime = currentTime - lastDrawTimeRef.current;
-  
+
       if (elapsedTime > redrawIntervalRef.current) {
         redrawIntervalRef.current = 20000;
 
-        canvas.width = canvasWidth ?? window.innerWidth;
-        canvas.height = canvasHeight ?? window.innerHeight;
-  
+        canvas.width = canvasWidth ?? document.documentElement.clientWidth;
+        canvas.height = canvasHeight ?? document.documentElement.clientHeight;
+
         const vertices = previousVerticesRef.current || [];
         if (vertices.length !== 16) {
           for (let i = 0; i < 16; i++) {
@@ -44,15 +48,15 @@ const ShapeCanvas: React.FC<ShapeCanvasProps> = ({ className = '', canvasWidth, 
             };
           }
         }
-  
+
         const targetVertices = Array.from({ length: 16 }, () => ({
           x: Math.random() * canvas.width,
           y: Math.random() * canvas.height,
         }));
-  
+
         let startTime = currentTime;
         const duration = 10000;
-  
+
         const tween = () => {
           const now = performance.now();
           const progress = Math.min((now - startTime) / duration, 1);
@@ -60,20 +64,24 @@ const ShapeCanvas: React.FC<ShapeCanvasProps> = ({ className = '', canvasWidth, 
             x: v.x + (targetVertices[i].x - v.x) * progress,
             y: v.y + (targetVertices[i].y - v.y) * progress,
           }));
-  
+
           ctx.clearRect(0, 0, canvas.width, canvas.height);
 
           // Fond blanc
-          ctx.fillStyle = 'white';
+          ctx.fillStyle = "#F4F3EF";
           ctx.fillRect(0, 0, canvas.width, canvas.height);
-  
+
           // Dégradé radial pour la forme avec les couleurs et l'opacité
           const gradient = ctx.createRadialGradient(
-            canvas.width / 2, canvas.height / 2, 0, 
-            canvas.width / 2, canvas.height / 2, canvas.width / 2
+            canvas.width / 2,
+            canvas.height / 2,
+            0,
+            canvas.width / 2,
+            canvas.height / 2,
+            canvas.width / 2,
           );
-          gradient.addColorStop(0, 'rgba(43, 74, 251, 0.46)'); // Couleur centrale avec opacité
-          gradient.addColorStop(1, 'rgba(17, 112, 255, 0.46)'); // Couleur extérieure avec opacité
+          gradient.addColorStop(0, "rgba(210, 250, 82, 0.46)"); // Couleur centrale avec opacité
+          gradient.addColorStop(1, "rgba(210, 250, 82, 0.46)"); // Couleur extérieure avec opacité
 
           // Applique le dégradé
           ctx.beginPath();
@@ -82,9 +90,9 @@ const ShapeCanvas: React.FC<ShapeCanvasProps> = ({ className = '', canvasWidth, 
           ctx.closePath();
           ctx.fillStyle = gradient;
           ctx.fill();
-  
+
           // Application du flou
-          ctx.filter = 'blur(100px)';
+          ctx.filter = "blur(100px)";
           ctx.fillStyle = gradient;
           ctx.fill();
 
@@ -95,19 +103,24 @@ const ShapeCanvas: React.FC<ShapeCanvasProps> = ({ className = '', canvasWidth, 
             previousVerticesRef.current = targetVertices;
           }
         };
-  
+
         tween();
         lastDrawTimeRef.current = currentTime;
       }
     };
-  
+
     animationFrameId = requestAnimationFrame(draw);
-  
+
     return () => cancelAnimationFrame(animationFrameId);
   }, []);
-  
 
-  return <canvas ref={canvasRef} className={className} />;
+  return (
+    <canvas
+      ref={canvasRef}
+      className={`${className}`}
+      style={{ maxWidth: "100vw", display: "block" }}
+    />
+  );
 };
 
 export default ShapeCanvas;
